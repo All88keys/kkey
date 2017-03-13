414f695fed2a373f1013229e7eea69e6cae11cd4
All88keys <chrisvass1@gmail.com>
2017-03-13 10:12:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.getElementById('pressed').innerHTML = '5';
+var times= 0;
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+
+   if (key == 75) {
+     times++;
+     alert(times);
+   }
+}
+//document.getElementById('pressed').innerHTML = '5';
