d819552699d37b611f45f56b6378da6e8c4b9219
All88keys <chrisvass1@gmail.com>
2017-03-16 10:37:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var times= 0;
+var audio = new Audio('ding.mp3');
+
+
 window.onkeyup = function(e) {
    var key = e.keyCode ? e.keyCode : e.which;
 
    if (key == 75) {
      times++;
+     audio.play();
      document.getElementById('pressed').innerHTML = times;
    }
 }
