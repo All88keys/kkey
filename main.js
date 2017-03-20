9df46079df85508f9acf2c7ead978bf63e9787a0
All88keys <chrisvass1@gmail.com>
2017-03-20 14:01:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ window.onkeyup = function(e) {
      document.getElementById('pressed').innerHTML = times;
    }
 }
+
+i = 60;
+function onTimer() {
+  document.getElementById('mycounter').innerHTML = i;
+  i--;
+  if (i < 0) {
+    alert('You lose!');
+  }
+  else {
+    setTimeout(onTimer, 1000);
+  }
+}
