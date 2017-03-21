b77728f1f4f0607f2ec576f64f279e50436c29a7
All88keys <chrisvass1@gmail.com>
2017-03-21 10:37:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var i = 60;
 var times= 0;
 var audio = new Audio('ding.mp3');
+var countingDown = false;
 
 
 window.onkeypress = function(e) {
    var key = e.keyCode ? e.keyCode : e.which;
-   i = 60;
-   onTimer();
+   if(!countingDown){
+      i = 60;
+      onTimer();
+      countingDown = true;
+   }
 
    if (key == 75) {
      audio.pause();
@@ -21,6 +25,8 @@ function onTimer() {
   i-0.1;
   if (i < 0) {
     alert('You lose!');
+    countingDown = false;
+    break;
   }
   else {
     setTimeout(onTimer, 100);
