f2cb5aa45aa2d85fd140308cfc00c9cf64d7f70e
All88keys <chrisvass1@gmail.com>
2017-03-22 08:21:42 -0400

    Update osu.js

--- osu.js
+++ osu.js
@@ -25,7 +25,7 @@ function onTimer() {
   document.getElementById('mycounter').innerHTML = i;
   i--;
   if (i < 0) {
-    alert('TIME UP BITCH');
+    alert('TIME UP');
     countingDown = false;
     times = 0;
   }
